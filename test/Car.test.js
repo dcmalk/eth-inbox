9912ff0a48d8c4554b42a03cdf97360c6aa8e622
Damon Malkiewicz <damon@newexpanse.com>
2021-11-17 10:40:04 -0500

    First commit

---
+++ test/Car.test.js
@@ -0,0 +1,30 @@
+/*const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider()); // provider defines the network you want to connect to
+
+class Car {
+  park() {
+    return 'stopped';
+  }
+  drive() {
+    return 'vroom';
+  }
+}
+
+describe('Car Class', () => {
+  let car;
+
+  beforeEach(() => {
+    car = new Car();
+  });
+
+  it('can park', () => {
+    assert.equal(car.park(), 'stopped');
+  });
+
+  it('can drive', () => {
+    assert.equal(car.drive(), 'vroom');
+  });
+});
+*/
