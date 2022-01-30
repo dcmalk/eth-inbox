bc41ddda3b531f52e5228e1418ebcb601fc282d1
Damon Malkiewicz <damon@newexpanse.com>
2022-01-30 16:32:38 -0500

    Update to solc 0.8.9

--- deploy.js
+++ deploy.js
@@ -1,10 +1,10 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
+const { abi, evm } = require('./compile');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const provider = new HDWalletProvider(process.env.NEMONIC_PHRASE, process.env.NETWORK_URL);
+provider = new HDWalletProvider(process.env.NEMONIC_PHRASE, process.env.NETWORK_URL);
 const web3 = new Web3(provider);
 
 const deploy = async () => {
@@ -12,11 +12,12 @@ const deploy = async () => {
 
   console.log('Attempting to deploy from account', accounts[0]);
 
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
     .send({ from: accounts[0], gas: '1000000' });
 
   console.log('Contract deployed to', result.options.address);
   provider.engine.stop(); // prevents hanging deployment
 };
+
 deploy();
